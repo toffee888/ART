865e44993701e28a60e83326e86f568768ff7d0f
toffee888 <86227265+toffee888@users.noreply.github.com>
2021-10-17 18:56:07 +0700

    Update click-handler.js

--- click-handler.js
+++ click-handler.js
@@ -1,7 +1,7 @@
 AFRAME.registerComponent('clickhandler', {
   init: function () {
     this.el.addEventListener('click', function (evt) {
-      var b = document.querySelector('#banner');
+      var b = document.querySelector('#banner2');
       b.setAttribute('visible',false)     
       }
     });
