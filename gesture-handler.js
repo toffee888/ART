b7bbb40e5a6e654f1ed6a18a07a15423f07e9238
toffee888 <86227265+toffee888@users.noreply.github.com>
2021-10-26 13:05:09 +0700

    Update gesture-handler.js

--- gesture-handler.js
+++ gesture-handler.js
@@ -3,18 +3,16 @@
 AFRAME.registerComponent('gesture-handler', {
     schema: {
         enabled: { default: true },
-        rotationFactor: { default: 5 },
-        minScale: { default: 0.3 },
-        maxScale: { default: 20 },
+        minScale: { default: 0.08 },
+        maxScale: { default: 8 },
     },
 
     init: function() {
         this.handleScale = this.handleScale.bind(this);
-        this.handleRotation = this.handleRotation.bind(this);
-
-        this.isVisible = true;
         this.initialScale = this.el.object3D.scale.clone();
         this.scaleFactor = 1;
+        this.isVisible = true;
+
     },
 
     update: function() {
